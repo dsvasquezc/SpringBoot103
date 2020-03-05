a6ede7ee9c48855934c83d116e2c3502d45bb798
dsvasquezc <dsvasquezc@gmail.com>
2020-03-04 20:53:20 -0500

    Initial commit

---
+++ src/test/java/com/david/springboot/Springboot103ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot103ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
